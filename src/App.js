05f7dae471a41b95f00b148b5f92cfdcaaa0c7d5
abdulraheem88 <168866788+abdulraheem88@users.noreply.github.com>
2024-08-04 11:29:42 -0700

    Normalize all line endings

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Alert from './components/Alert';
 import TextForm from './components/TextForm';
 import About from './components/About';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
@@ -25,23 +27,27 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
-     // document.title = 'TextUtils - Dark Mode'
+      // document.title = 'TextUtils - Dark Mode'
     } else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-     // document.title = 'TextUtils - Light Mode'
+      // document.title = 'TextUtils - Light Mode'
     }
   };
 
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <div className="container my-3">
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
-        <About/>
-      </div>
+        <div className="container my-3">
+          <Routes>
+            <Route path='/about' element={<About />} />
+            <Route path='/' element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
